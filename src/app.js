d9ff10f12eeee584cdc05f92eeabefa05302af0b
Matt Gibney <matt@nodeten.co.uk>
2020-10-29 20:34:26 +0000

    Added some documentation for the healthcheck controller. Added some documentation for the addition of Config to the request object.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const app = express();
 
 const healthcheckController = require('./controllers/healthcheck');
 
+/**
+ * Adding the config to the request object here makes it available across ALL
+ * requests that we add in the future. This removes the need to `require` the
+ * config wherever it's required. 
+ */
 app.use((req, res, next) => {
   req.Config = config;
   next();
--- src/controllers/healthcheck.js
+++ src/controllers/healthcheck.js
@@ -1,5 +1,13 @@
 module.exports = {
+  /**
+   * Returns a JSON object containing the version number of the application.
+   * This can be used by a load balancer to confirm that the application is both
+   * running properly and that it is running the expected version.
+   *
+   * @param {*} req 
+   * @param {*} res 
+   */
   fetchHealthcheck(req, res) {
-    res.send({ version: req.Config.version });
+    return res.status(200).json({ version: req.Config.version });
   }
 };
