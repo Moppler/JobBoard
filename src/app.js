a79b43249355c6dc1be90e6bad9ef7f70e46b1bc
Matt Gibney <matt@nodeten.co.uk>
2020-10-31 23:49:12 +0000

    Added type definition for Request object to improve intellisense

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {object} _JobBoardRequest
+ * @property {import('../config/environment')} Config - App Config
+ * @property {import('./modelFactory')} ModelFactory - Instance of
+ */
+
+/**
+ * @typedef {import('express').Request & _JobBoardRequest} JBRequest
+ */
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 
@@ -5,14 +15,18 @@ const config = require('../config/environment');
 const app = express();
 
 const router = require('./router');
+const ModelFactory = require('./modelFactory');
 
 /**
  * Adding the config to the request object here makes it available across ALL
  * requests that we add in the future. This removes the need to `require` the
  * config wherever it's required.
+ *
+ * The ModelFactory provides an easy way to access the application's models.
  */
 app.use((req, res, next) => {
   req.Config = config;
+  req.ModelFactory = ModelFactory;
   next();
 });
 
