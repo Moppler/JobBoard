1813cd0733bdaac073a28761cabccecac7606c53
Matt Gibney <matt@nodeten.co.uk>
2020-11-01 11:04:42 +0000

    Switched assertion library to chai

--- test/models/job.spec.js
+++ test/models/job.spec.js
@@ -1,5 +1,5 @@
-/** @type {any} */
-const assert = require('assert');
+const assert = require('chai').assert;
+const sinon = require('sinon');
 
 const JobModel = require('../../src/models/job');
 
@@ -18,12 +18,46 @@ describe('Model: Job', function () {
   describe('Static Functions', function () {
     describe('fetchAllJobs', function () {
       it('returns new instances of the JobModel', async function () {
-        const jobs = await JobModel.fetchAllJobs();
+        const mockDaoFactory = {
+          job: {
+            fetchAllJobs: sinon.stub().resolves([
+              {
+                title: 'Test Title',
+                location: 'London',
+                salary: '£50,000',
+                jobType: 'Temporary',
+                summary: 'Lorem ipsum',
+                datePosted: '',
+              },
+            ]),
+          },
+        };
+        const jobs = await JobModel.fetchAllJobs({}, mockDaoFactory);
 
         assert.ok(
           jobs.every((job) => job instanceof JobModel),
           'Every item in the array returned is an instance of JobModel'
         );
+      });
+      it('returns empty array when dao returns no data', async function () {
+        const mockDaoFactory = {
+          job: {
+            fetchAllJobs: sinon.stub().resolves([]),
+          },
+        };
+        const jobs = await JobModel.fetchAllJobs({}, mockDaoFactory);
+
+        assert.deepStrictEqual(jobs, [], 'Returns null on error');
+      });
+      it('returns null when the dao returns null', async function () {
+        const mockDaoFactory = {
+          job: {
+            fetchAllJobs: sinon.stub().resolves(null),
+          },
+        };
+        const jobs = await JobModel.fetchAllJobs({}, mockDaoFactory);
+
+        assert.strictEqual(jobs, null, 'Returns null on error');
       });
     });
   });
