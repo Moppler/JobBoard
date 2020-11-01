63aacbbcfe9b257be94981a361811a14c3034a04
Matt Gibney <matt@nodeten.co.uk>
2020-11-01 11:19:37 +0000

    Added some additional documentation for Daos and Models

--- src/modelFactory.js
+++ src/modelFactory.js
@@ -1,3 +1,14 @@
+/**
+ * ## What is a Model?
+ * Models are where we put all of our business logic. We create one for each
+ * major component of our application. In this Job Board app, we will see models
+ * for Jobs, Users, Applications. We may also want to model items that are
+ * shared across jobs, Location and Industry for example.
+ *
+ * The model represents a business concern and acts as a central location for
+ * working with them.
+ */
+
 const JobModel = require('./models/job');
 
 module.exports = {
