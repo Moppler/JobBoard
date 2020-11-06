fef5c0f9cc11c4477dd8baf6c674b245f32738fa
Matt Gibney <matt@nodeten.co.uk>
2020-11-06 18:19:00 +0000

    Added type definitions for jobs

--- src/daos/job.js
+++ src/daos/job.js
@@ -1,6 +1,21 @@
 /**
  * @typedef {import('../stores/job')} JobStore
+ * @typedef {import('../stores/job').JobRow} JobRow
  */
+
+/**
+ * @typedef {object} JobData
+ * @property {number} id
+ * @property {string} title
+ * @property {string} location
+ * @property {string} salary
+ * @property {string} jobType
+ * @property {string} summary
+ * @property {string} description
+ * @property {DateTime} datePosted
+ */
+
+const { DateTime } = require('luxon');
 
 class JobDao {
   /**
@@ -10,17 +25,44 @@ class JobDao {
     this.JobStore = JobStore;
   }
 
+  /**
+   * Converts a JobRow object to a JobData object.
+   *
+   * @param {JobRow} jobRow
+   * @returns {JobData}
+   */
+  _JobRowtoJobData(jobRow) {
+    return {
+      id: jobRow.id,
+      title: jobRow.title,
+      location: jobRow.location,
+      salary: jobRow.salary,
+      jobType: jobRow.jobType,
+      summary: jobRow.summary,
+      description: jobRow.description,
+      datePosted: DateTime.fromSQL(jobRow.datePosted),
+    };
+  }
+
+  /**
+   * Fetches all jobs from the store and returns an array of JobData objects.
+   *
+   * @returns {Promise<JobData[]>}
+   */
   async fetchAllJobs() {
-    return await this.JobStore.fetchAllJobs();
+    const jobRows = await this.JobStore.fetchAllJobs();
+    return jobRows.map((row) => this._JobRowtoJobData(row));
   }
 
   /**
    * Returns a job that matches the specified Id.
    *
    * @param {number} jobId
+   * @returns {Promise<JobData>}
    */
   async fetchJobById(jobId) {
-    return await this.JobStore.fetchJobById(jobId);
+    const jobRow = await this.JobStore.fetchJobById(jobId);
+    return this._JobRowtoJobData(jobRow);
   }
 }
 
--- src/stores/job.js
+++ src/stores/job.js
@@ -1,5 +1,16 @@
-const { DateTime } = require('luxon');
+/**
+ * @typedef {object} JobRow
+ * @property {number} id
+ * @property {string} title
+ * @property {string} location
+ * @property {string} salary
+ * @property {string} jobType
+ * @property {string} summary
+ * @property {string} description
+ * @property {string} datePosted - SQL datetime
+ */
 
+/** @type {JobRow[]} */
 const mockJobData = [
   {
     id: 1,
@@ -10,7 +21,7 @@ const mockJobData = [
     summary: 'Lorem ipsum dolor sit amet.',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam semper at nunc vel congue. Nulla vel aliquet metus. Nam at consequat nulla. Nullam tortor odio, vulputate et nunc at, interdum volutpat massa. Fusce id porta ligula. Mauris ultrices, nibh vel tristique vestibulum, risus dui ornare purus, vel bibendum enim odio vitae nulla. Pellentesque vehicula, nunc eu interdum egestas, ex diam gravida nibh, quis vehicula nisl est sed dui. Donec libero nibh, dapibus et est at, tincidunt maximus magna. Etiam vehicula suscipit metus. Cras ut sapien blandit, tempus tortor ac, eleifend sem. Maecenas bibendum metus quis venenatis tempus.',
-    datePosted: DateTime.local(2020, 1, 1),
+    datePosted: '2020-01-01',
   },
   {
     id: 2,
@@ -21,19 +32,25 @@ const mockJobData = [
     summary: 'Lorem ipsum dolor sit amet.',
     description:
       'Nam arcu justo, rutrum ut tortor sagittis, facilisis dignissim tortor. Vestibulum finibus mi a nunc dictum, sit amet tempor orci iaculis. Praesent tincidunt interdum urna, ut viverra justo interdum a. Duis ut dui sit amet augue consequat commodo. Duis sapien eros, efficitur at ultricies quis, fermentum sit amet velit. Aliquam ante odio, vulputate ac efficitur sit amet, imperdiet ac felis. Suspendisse quis felis luctus, rutrum arcu sed, tempor lacus. Donec malesuada dolor augue, viverra volutpat nisl placerat sit amet. Duis aliquam ligula eget commodo volutpat.',
-    datePosted: DateTime.local(2020, 1, 2),
+    datePosted: '2020-01-02',
   },
 ];
 
 class JobStore {
+  /**
+   * Fetches all jobs from the store.
+   *
+   * @returns {Promise<JobRow[]>}
+   */
   async fetchAllJobs() {
-    return await mockJobData;
+    return mockJobData;
   }
 
   /**
    * Returns a job that matches the specified Id.
    *
    * @param {number} jobId
+   * @returns {Promise<JobRow>}
    */
   async fetchJobById(jobId) {
     return mockJobData.find((job) => job.id === jobId);
