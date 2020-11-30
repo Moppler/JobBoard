bcecadd2d612947c5b42e26096b7d599318a9afa
Matt Gibney <matt@nodeten.co.uk>
2020-11-30 15:00:08 +0000

    Improving test coverage

--- test/models/user.spec.js
+++ test/models/user.spec.js
@@ -36,6 +36,68 @@ describe('Model: User', function () {
 
         assert.equal(fetchedUser, null);
       });
+      it('returns returns a new instance of the UserModel', async function () {
+        const mockFetchByEmail = sinon.stub().resolves({});
+
+        const mockModelFactory = {};
+        const mockDaoFactory = {
+          user: {
+            fetchByEmail: mockFetchByEmail,
+          },
+        };
+        const fetchedUser = await UserModel.fetchUserByEmail(
+          mockModelFactory,
+          mockDaoFactory,
+          ''
+        );
+
+        assert.instanceOf(fetchedUser, UserModel);
+        assert.deepEqual(fetchedUser.ModelFactory, mockModelFactory);
+        assert.deepEqual(fetchedUser.DaoFactory, mockDaoFactory);
+      });
+    });
+    describe('fetchUserById', function () {
+      it('returns null when the dao doesnt return a user', async function () {
+        const mockModelFactory = {};
+        const mockDaoFactory = {
+          user: {
+            fetchById: sinon.stub().resolves(null),
+          },
+        };
+        const fetchedUser = await UserModel.fetchUserById(
+          mockModelFactory,
+          mockDaoFactory,
+          ''
+        );
+
+        assert.equal(fetchedUser, null);
+      });
+      it('returns returns a new instance of the UserModel', async function () {
+        const mockModelFactory = {};
+        const mockDaoFactory = {
+          user: {
+            fetchById: sinon.stub().resolves({}),
+          },
+        };
+        const fetchedUser = await UserModel.fetchUserById(
+          mockModelFactory,
+          mockDaoFactory,
+          ''
+        );
+
+        assert.instanceOf(fetchedUser, UserModel);
+        assert.deepEqual(fetchedUser.ModelFactory, mockModelFactory);
+        assert.deepEqual(fetchedUser.DaoFactory, mockDaoFactory);
+      });
+    });
+    describe('verifyJWT', function () {
+      const decodedToken = UserModel.verifyJWT(
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+        {
+          secret: 'SECRET',
+        }
+      );
+      assert.equal(decodedToken, null);
     });
   });
 });
--- test/stores/user.spec.js
+++ test/stores/user.spec.js
@@ -16,4 +16,68 @@ describe('Store: User', function () {
       );
     });
   });
+  describe('fetchByEmail', function () {
+    it('catches exceptions from the DB and returns null', async function () {
+      const mockKnex = sinon.stub().throwsException();
+      const store = new UserStore(mockKnex);
+
+      const result = await store.fetchByEmail('');
+
+      assert.equal(result, null);
+    });
+    it('calls the database correctly', async function () {
+      const whereStub = sinon.stub().returns('ROWSHERE');
+      const mockKnex = sinon.stub().returns({
+        first: sinon.stub().returns({
+          where: whereStub,
+        }),
+      });
+      const store = new UserStore(mockKnex);
+
+      const result = await store.fetchByEmail('EMAILADDRESS');
+
+      assert.equal(
+        mockKnex.getCall(0).args[0],
+        'users',
+        'Correct table is called'
+      );
+
+      assert.equal(whereStub.getCall(0).args[0], 'email');
+      assert.equal(whereStub.getCall(0).args[1], 'EMAILADDRESS');
+
+      assert.equal(result, 'ROWSHERE');
+    });
+  });
+  describe('fetchById', function () {
+    it('catches exceptions from the DB and returns null', async function () {
+      const mockKnex = sinon.stub().throwsException();
+      const store = new UserStore(mockKnex);
+
+      const result = await store.fetchById('');
+
+      assert.equal(result, null);
+    });
+    it('calls the database correctly', async function () {
+      const whereStub = sinon.stub().returns('ROWSHERE');
+      const mockKnex = sinon.stub().returns({
+        first: sinon.stub().returns({
+          where: whereStub,
+        }),
+      });
+      const store = new UserStore(mockKnex);
+
+      const result = await store.fetchById('ID');
+
+      assert.equal(
+        mockKnex.getCall(0).args[0],
+        'users',
+        'Correct table is called'
+      );
+
+      assert.equal(whereStub.getCall(0).args[0], 'id');
+      assert.equal(whereStub.getCall(0).args[1], 'ID');
+
+      assert.equal(result, 'ROWSHERE');
+    });
+  });
 });
